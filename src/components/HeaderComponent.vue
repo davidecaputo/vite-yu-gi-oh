04e1e9c6150ab8a8cec2171026206977b438227d
davidecaputo <daca9621@gmail.com>
2023-04-18 17:29:44 +0200

    Add cards

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,30 @@
 <template>
-    
+    <div class="d-flex flex-wrap container">
+        {{ prova.card_images[0].image_url }}
+    </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        
+        name: 'HeaderComponent',
+        data(){
+            return {
+                    prova: []
+                }
+        },
+        methods: {
+            getElements(){
+                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=').then((el) => {
+                    console.log(el.data.data[0]);
+                    this.prova = el.data.data[0];
+                });
+            }
+        },
+        mounted(){
+            this.getElements();
+            console.log(this.prova);
+        }
     }
 </script>
 
