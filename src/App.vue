8865f3d6bb1edaaa3617c32c0e70688c0877ed1b
davidecaputo <daca9621@gmail.com>
2023-04-18 09:34:14 +0200

    Add HeaderComponent and MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+// export default {
+//     name: 'App',
+//     components: {
+//         HeaderComponent,
+//         MainComponent
+//     }
+// }
 </script>
 
 <template>
-
+<HeaderComponent />
+<MainComponent />
 </template>
+
 
 <style scoped>
 
