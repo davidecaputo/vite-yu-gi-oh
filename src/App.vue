c9a99edaec4b7d7b3ed64e8fdb22e14bb79f8744
davidecaputo <daca9621@gmail.com>
2023-04-19 01:18:58 +0200

    Add cards

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
-<script setup>
+<script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
-// export default {
-//     name: 'App',
-//     components: {
-//         HeaderComponent,
-//         MainComponent
-//     }
-// }
+import axios from 'axios'
+import { store } from './data/store';
+export default {
+    name: 'App',
+    components: {
+        HeaderComponent,
+        MainComponent
+    },
+    data(){
+        return {
+            store
+        }
+    },
+    methods: {
+        getElements(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=').then((el) => {
+                console.log(el.data.data[0]);
+                store.cardElements = el.data.data;
+            });
+        }
+    },
+    mounted(){
+        this.getElements();
+    }
+}
 </script>
 
 <template>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,40 @@
 <template>
-   
+    <main>
+        <div class="container p-5 bg-white rounded-4">
+            <div class="row gap-4 d-flex justify-content-center">
+                <div v-for="card in store.cardElements" class="col-12 col-sm-6 col-md-4 col-lg-2">
+                    <div class="card">
+                        <img :src="card.card_images[0].image_url" :alt="card.name">
+                        <div class="card-body dc-card-body text-center">
+                            <h5 class="text-white">Nome:</h5>
+                            <h5 class="text-white">{{ card.name }}</h5>
+                            <span class="pt-4 fw-bold" v-if="card.archetype">Archetype:</span>
+                            <p>{{ card.archetype }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
+    import { store } from '../data/store';
     export default {
-        name: 'MainComponent'
-    }
+        name: 'MainComponent',
+        data() {
+            return {
+                store
+            }
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
-
+    main{
+        background-color: #048f7c;
+    }
+    .dc-card-body{
+        background-color: #d48f38;
+    }
 </style>
